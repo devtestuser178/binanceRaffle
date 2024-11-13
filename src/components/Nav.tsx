748812521820127b56a033317456bc727acda7e5
AnthonyChablov <achablov@myseneca.ca>
2024-11-13 13:12:55 -0500

    feat: make nav responsive include sheet

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,11 +2,12 @@
 import React from "react";
 import { motion } from "framer-motion";
 import binanceLogo from "../assets/binance-h.png";
-import Container from "./Container";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import displayIcon from "../assets/icons/display-icon.svg";
 import nightIcon from "../assets/icons/night-icon.svg";
+import menuIcon from "../assets/icons/menu.svg";
 import Link from "next/link";
 import {
   Sheet,
@@ -16,6 +17,20 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+
+interface AuthButtonProps {
+  href: string;
+  children: string;
+  className: string;
+}
+
+export const AuthButton = ({ href, children, className }: AuthButtonProps) => {
+  return (
+    <Button className={cn(`${className}`)} asChild>
+      <Link href={href}>{children}</Link>
+    </Button>
+  );
+};
 
 export const Nav = () => {
   return (
@@ -38,31 +53,45 @@ export const Nav = () => {
           width={125}
           height={125}
         />
-        <div className="space-x-2 flex items-center">
-          <Button
+        <div className="h-fit flex items-center md:hidden">
+          <Sheet>
+            <SheetTrigger className="hover:bg-transparent bg-transparent hover:text-customYellow ">
+              <Image width={30} height={30} src={menuIcon} alt="Display Icon" />
+            </SheetTrigger>
+            <SheetContent className="bg-customBlack">
+              <SheetHeader>
+                <SheetTitle></SheetTitle>
+                <SheetDescription></SheetDescription>
+              </SheetHeader>
+              <AuthButton
+                className="bg-customGrey hover:bg-customGrey hover:backdrop-blur-md text-white   text-sm font-semibold hover:filter hover:brightness-90"
+                href="https://accounts.binance.com/en/login?loginChannel=&return_to="
+              >
+                Log In
+              </AuthButton>
+              <AuthButton
+                className="bg-customYellow hover:bg-customYellow hover:backdrop-blur-md text-customBlack text-sm font-semibold hover:filter hover:brightness-90"
+                href="https://accounts.binance.com/en/register?registerChannel=&return_to="
+              >
+                Sign Up
+              </AuthButton>
+            </SheetContent>
+          </Sheet>
+        </div>
+
+        <div className="space-x-2  hidden md:flex md:items-center ">
+          <AuthButton
             className="bg-customGrey hover:bg-customGrey hover:backdrop-blur-md text-white   text-sm font-semibold hover:filter hover:brightness-90"
-            asChild
+            href="https://accounts.binance.com/en/login?loginChannel=&return_to="
           >
-            <Link
-              href={
-                "https://accounts.binance.com/en/login?loginChannel=&return_to="
-              }
-            >
-              Log In
-            </Link>
-          </Button>
-          <Button
+            Log In
+          </AuthButton>
+          <AuthButton
             className="bg-customYellow hover:bg-customYellow hover:backdrop-blur-md text-customBlack text-sm font-semibold hover:filter hover:brightness-90"
-            asChild
+            href="https://accounts.binance.com/en/register?registerChannel=&return_to="
           >
-            <Link
-              href={
-                "https://accounts.binance.com/en/register?registerChannel=&return_to="
-              }
-            >
-              Sign Up
-            </Link>
-          </Button>
+            Sign Up
+          </AuthButton>
           <Button
             className="hover:bg-transparent bg-transparent hover:text-customYellow "
             size="icon"
